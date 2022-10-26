c8a6d7caa91ca57a9d99a0a04fbd2f6b38e90d32
Goenjoy Huang <goenjoy@khadas.com>
2022-10-26 09:44:14 +0800

    LCD: Compatible with TS050 and TS101 [4/4] u-boot [1/4] kernel-5.10 [2/4] system/core [3/4]
    Signed-off-by: Goenjoy Huang <goenjoy@khadas.com>

--- kedge2/kedge2.mk
+++ kedge2/kedge2.mk
@@ -42,12 +42,7 @@ PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=240
 PRODUCT_PROPERTY_OVERRIDES += ro.wifi.sleep.power.down=true
 PRODUCT_PROPERTY_OVERRIDES += persist.wifi.sleep.delay.ms=0
 PRODUCT_PROPERTY_OVERRIDES += persist.bt.power.down=true
-PRODUCT_PROPERTY_OVERRIDES += vendor.hwc.device.primary=HDMI-A,DP
-PRODUCT_PROPERTY_OVERRIDES += vendor.hwc.device.extend=DSI
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.rotation.efull=true
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.rotation.einit-1=3
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.rotation.einit-2=3
-PRODUCT_PROPERTY_OVERRIDES += persist.vendor.framebuffer.main=1920x1080@60
 
 PRODUCT_PROPERTY_OVERRIDES += service.adb.tcp.port=5555
 BUILD_NUMBER2 := $(shell $(DATE) +%Y%m%d)
