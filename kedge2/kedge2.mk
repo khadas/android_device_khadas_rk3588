3c8659377f93ee2d1ce50250338dde05c2b25298
Goenjoy Huang <goenjoy@khadas.com>
2022-12-15 11:10:57 +0800

    set ro.sf.lcd_density = 240 default
    Signed-off-by: Goenjoy Huang <goenjoy@khadas.com>

--- kedge2/kedge2.mk
+++ kedge2/kedge2.mk
@@ -44,7 +44,7 @@ PRODUCT_COPY_FILES += \
 #
 ## add Rockchip properties
 #
-PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=320
+PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=240
 PRODUCT_PROPERTY_OVERRIDES += ro.wifi.sleep.power.down=true
 PRODUCT_PROPERTY_OVERRIDES += persist.wifi.sleep.delay.ms=0
 PRODUCT_PROPERTY_OVERRIDES += persist.bt.power.down=true
